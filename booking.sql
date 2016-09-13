896f2edc499ccd684d5013ceb38e95b4ec7d8c18
fishyFrogFace <camillahallo@mail.com>
2016-09-13 23:53:33 +0200

    Added database schema and content

---
+++ booking.sql
@@ -0,0 +1,87 @@
+CREATE TABLE paygrade (
+    paygradeID SERIAL,
+    perHour INT NOT NULL,
+    PRIMARY KEY(paygradeID));
+
+CREATE TYPE role AS ENUM 
+    ('tech', 'organizer', 'booker', 'administrator');
+
+CREATE TABLE employee (
+    employeeID SERIAL,
+    userName TEXT NOT NULL,
+    name TEXT NOT NULL,
+    password TEXT NOT NULL,
+    accountType ROLE NOT NULL,
+    paygradeID INT REFERENCES paygrade(paygradeID),
+    PRIMARY KEY(employeeID));
+
+CREATE TABLE stage (
+    stageID SERIAL,
+    name TEXT,
+    maxAudience INT NOT NULL,
+    PRIMARY KEY(stageID));
+
+CREATE TYPE musicGenre AS ENUM
+    ('rap', 'pop', 'rock', 'blues', 'alternative', 
+    'country', 'electronic', 'dance', 'classical', 
+    'r&b/soul', 'reggae', 'jazz', 'metal');
+
+CREATE TABLE artist (
+    artistID SERIAL,
+    name TEXT NOT NULL,
+    email TEXT,
+    fee INT,
+    genre musicGenre NOT NULL,
+    accomodationCost INT,
+    PRIMARY KEY(artistID));
+    
+CREATE TABLE artistinfo (
+    infoID SERIAL,
+    spotify INT,
+    albumsSold INT,
+    concerts TEXT,
+    artistID INT NOT NULL REFERENCES artist(artistID),
+    PRIMARY KEY(infoID));
+
+CREATE TYPE offerState AS ENUM
+    ('unfinished', 'pending', 'accepted', 
+    'declined', 'sent', 'returned', 'archived');
+
+CREATE TABLE offer (
+    offerID SERIAL,
+    emailSubject TEXT NOT NULL,
+    emailBody TEXT NOT NULL,
+    date DATE NOT NULL,
+    state offerState NOT NULL,
+    artistID INT NOT NULL REFERENCES artist(artistID),
+    bookerID INT NOT NULL REFERENCES employee(employeeID),
+    managerID INT REFERENCES employee(employeeID),
+    stageID INT NOT NULL REFERENCES stage(stageID),
+    PRIMARY KEY(offerID));
+
+CREATE TABLE event (
+    eventID SERIAL,
+    startDate DATE NOT NULL,
+    duration INT,
+    ticketPrice INT,
+    ticketsSold INT,
+    artistID INT NOT NULL REFERENCES artist(artistID),
+    offerID INT REFERENCES offer(offerID),
+    stageID INT NOT NULL REFERENCES stage(stageID),
+    PRIMARY KEY(eventID));
+
+CREATE TABLE assigned (
+    assignedID SERIAL,
+    hours INT,
+    employeeID INT REFERENCES employee(employeeID)
+    ON DELETE CASCADE,
+    eventID INT REFERENCES event(eventID)
+    ON DELETE CASCADE,
+    PRIMARY KEY(assignedID));
+    
+CREATE TABLE budgetPost (
+    budgetPostID SERIAL,
+    description TEXT NOT NULL,
+    price INT NOT NULL,
+    eventID INT REFERENCES event(eventID),
+    PRIMARY KEY(budgetPostID));
---
+++ content.sql
@@ -0,0 +1,26 @@
+INSERT INTO paygrade VALUES
+    (DEFAULT, 150),
+    (DEFAULT, 200),
+    (DEFAULT, 250),
+    (DEFAULT, 300);
+
+INSERT INTO employee VALUES
+    (DEFAULT, 'acme', 'Wile E. Coyote', '$2a$10$IjOmy7phDesVDYcxnTNJcOCEHb1/.OISWcpDCYpFyU./yENcGFkTK', 'administrator', 3),
+    (DEFAULT, 'meepMeep', 'Road Runner', '$2a$10$8xeq0y1bwjXRCPtyb0FZ8e/0ZgPOvoZGxANw8kU3Ad2aMLWyKKDyO', 'booker', 2),
+    (DEFAULT, 'greenCarrot', 'Bugs Bunny', '$2a$10$YACSS2P754s2l9euPn8IneYKcudp/jM9qpHWRZENhHn6krvcB6Nfu', 'organizer', 1),
+    (DEFAULT, 'mars', 'Marvin Martian', '$2a$10$IjOmy7phDesVDYcxnTNJcOCEHb1/.OISWcpDCYpFyU./yENcGFkTK', 'tech', 4);
+
+INSERT INTO stage VALUES
+    (DEFAULT, 'Storsalen', 1000),
+    (DEFAULT, 'Klubben', 400),
+    (DEFAULT, 'Knaus', 200),
+    (DEFAULT, 'Selskapssiden', 50),
+    (DEFAULT, 'Strossa', 150);
+
+INSERT INTO artist VALUES
+    (DEFAULT, 'Michigan J. Frog', 'prosjektarbeidartist@mail.com', 3000, 'classical', 3000),
+    (DEFAULT, 'Deathgore', 'prosjektarbeidartist2@mail.com', 4000, 'metal', 3000),
+    (DEFAULT, 'Money$$', 'prosjektarbeidartist3@mail.com', 10000, 'rap', 5000),
+    (DEFAULT, 'Super Mario', 'prosjektarbeidartist4@mail.com', 2000, 'electronic', 1000),
+    (DEFAULT, 'Bob Marley', 'prosjektarbeidartist5@mail.com', 20000, 'reggae', 4000),
+    (DEFAULT, 'Guy with guitar', 'prosjektarbeidartist6@mail.com', 7000, 'country', 2000);
