2e90c627b0c02c4fd223f69bdc4be5db2544436b
Lars Martin Aders <Lars Martin Aders>
2016-09-16 15:49:08 +0200

    Added som variables to test git

--- src/main/java/com/it1901/booking/Application/User.java
+++ src/main/java/com/it1901/booking/Application/User.java
@@ -1,4 +1,10 @@
 package com.it1901.booking.Application;
 
 public abstract class User {
+	private String name = null; 
+	private String password = null;
+	
+	User(String name, String password){
+		
+	}
 }
