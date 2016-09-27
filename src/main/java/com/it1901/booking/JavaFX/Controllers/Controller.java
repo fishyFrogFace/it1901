7ed615e36c303ab01d1a29731bad52532b9508a1
philippu <philippuente@hotmail.com>
2016-09-27 13:41:51 +0200

    La til navigasjonsbar for FXML sidene

--- src/main/java/com/it1901/booking/JavaFX/Controllers/Controller.java
+++ src/main/java/com/it1901/booking/JavaFX/Controllers/Controller.java
@@ -1,11 +1,28 @@
 package com.it1901.booking.JavaFX.Controllers;
 
 import com.it1901.booking.JavaFX.BookingApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
 
 public abstract class Controller {
     protected BookingApp app;
 
     public void setApp(BookingApp app) {
         this.app = app;
+    }
+
+    @FXML
+    private MenuItem menuDash;
+
+    @FXML
+    private MenuItem menuLogout;
+
+    public void goToDash(){
+        app.makeDash();
+    }
+
+    public void logOut() {
+        app.setUser(null);
+        app.makeLogin();
     }
 }
