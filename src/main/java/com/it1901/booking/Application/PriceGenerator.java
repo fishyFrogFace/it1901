5d2a8f9eaa48ddf7d5fbd951e1b1da3672d06b9f
Lars Martin Aders <Lars Martin Aders>
2016-09-30 14:44:42 +0200

    added method

--- src/main/java/com/it1901/booking/Application/PriceGenerator.java
+++ src/main/java/com/it1901/booking/Application/PriceGenerator.java
@@ -1,5 +1,15 @@
 package com.it1901.booking.Application;
 
 public class PriceGenerator {
-
+	
+	int ticketPrice;
+	//7. Som bookingsjef ønsker jeg å kunne få generert et forslag til billettpris som tar høyde for markedsinformasjon
+	//og faktiske kostnader, og få forslag til billettpris på scener med ulik størrelse slik at konserter går i økonomisk balanse.
+	PriceGenerator(){
+		
+	}
+	public float computeTicketPrice(float cost, String scene){
+		
+		return ticketPrice;
+	}
 }
