9c8384418c541960b044f63cbffa86f35c7cd8c1
fishyFrogFace <camillahallo@mail.com>
2016-10-03 23:56:07 +0200

    Restructure Calendar.java so it can return a GridPane

--- src/main/java/com/it1901/booking/JavaFX/Calendar/Calendar.java
+++ src/main/java/com/it1901/booking/JavaFX/Calendar/Calendar.java
@@ -8,113 +8,69 @@ import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 import com.it1901.booking.Application.DatabaseHandler;
-import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
-import javafx.stage.Stage;
 
 import com.it1901.booking.Application.Stage.stages;
 
-public class Calendar extends Application {
+public class Calendar {
 
-	@Override
-    //TODO break into smaller methods and remove main
-    //is going to be changed to a method that returns the calendar GridPane
-    //so it can be embedded in other elements as child
-	public void start(Stage primaryStage) {
-		DatabaseHandler dbh = new DatabaseHandler(
-				"org.postgresql.Driver",
-				"jdbc:postgresql://52.40.176.177:5432/booking",
-				"team",
-				"it1901");
+    private final LocalDate basis;
+    private final LocalDate startOfWeek;
+    private final LocalDate endOfWeek;
+    private GridPane calGrid = new GridPane();
 
-		GridPane calendar = new GridPane();
-        calendar.getStyleClass().addAll("pane", "grid");
+    public Calendar(LocalDate basis) {
+        this.basis = LocalDate.now();
+        this.startOfWeek = basis.minusDays(basis.getDayOfWeek().getValue() - 1);
+        this.endOfWeek = startOfWeek.plusDays(6);
+    }
 
-        //FIXME remove adding of a day to today
-		LocalDate today = LocalDate.now().plusDays(1);
-		LocalDate startOfWeek = today.minusDays(today.getDayOfWeek().getValue() - 1);
-		LocalDate endOfWeek = startOfWeek.plusDays(6);
+    //returns a calendar GridPane loaded with this week's concerts
+	public GridPane createCalendar(DatabaseHandler dbh) {
 
-		try {
-			ResultSet rs = getCalendarContent(today, startOfWeek, endOfWeek, dbh);
+        calGrid.getStyleClass().addAll("pane", "grid");
+
+        this.loadConcerts(dbh);
+        this.setDateLabels();
+        this.setStageLabels();
+
+		return calGrid;
+	}
+
+	private void loadConcerts(DatabaseHandler dbh) {
+        try {
+            ResultSet rs = getCalendarContent(basis, startOfWeek, endOfWeek, dbh);
             rs.next();
-			for (LocalDate date = startOfWeek; !date.isAfter(endOfWeek); date = date.plusDays(1)) {
-				int slotIndex = 1;
+            for (LocalDate date = startOfWeek; !date.isAfter(endOfWeek); date = date.plusDays(1)) {
+                int slotIndex = 1;
 
-				for (stages stage : stages.values()) {
-					VBox eventsToday = new VBox();
+                for (stages stage : stages.values()) {
+                    VBox eventsToday = new VBox();
                     eventsToday.getStyleClass().addAll("pane", "vbox");
 
-                    //loop through grid works now
                     while (
                             !rs.isAfterLast() &&
-                            rs.getDate(2).toLocalDate().equals(date) &&
-                            rs.getString(7).equals(stage.toString())
+                                    rs.getDate(2).toLocalDate().equals(date) &&
+                                    rs.getString(7).equals(stage.toString())
                             ) {
-                        System.out.println("2. Date: "+date+" startDate: "+rs.getDate(2).toLocalDate().toString());
-                        Button newBtn = new Button(' ' + rs.getInt(1) + " fdfk");
-                        String state = rs.getString(6);
-                        if (state.equals("pending")) {
-                            newBtn.setStyle("-fx-base: blue"); //TODO move to css
-                        }
-                        else if (state.equals("accepted")) {
-                            newBtn.setStyle("-fx-base: green");
-                        }
-                        else if (state.equals("sent")) {
-                            newBtn.setStyle("-fx-base: yellow");
-                        }
-                        else if (state.equals("declined")) {
-                            newBtn.setStyle("-fx-base: red");
-                        }
-                        eventsToday.getChildren().add(newBtn);
+                        eventsToday.getChildren().add(concertButton(eventsToday, rs));
                         rs.next();
                     }
 
-					calendar.add(eventsToday, date.getDayOfWeek().getValue(), slotIndex);
-					slotIndex++;
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-
-
-		//labels for dates (top) and stages (side)
-		DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("E\nMMM d");
-
-		for (LocalDate date = startOfWeek; !date.isAfter(endOfWeek); date = date.plusDays(1)) {
-			Label label = new Label(date.format(dayFormatter));
-			label.setPadding(new Insets(1));
-			label.setTextAlignment(TextAlignment.CENTER);
-			GridPane.setHalignment(label, HPos.CENTER);
-			calendar.add(label, date.getDayOfWeek().getValue(), 0);
-		}
-
-		int slotIndex = 1;
-		for (stages stage : stages.values()) {
-			Label label = new Label(stage.toString());
-			label.setPadding(new Insets(2));
-			GridPane.setHalignment(label, HPos.RIGHT);
-			calendar.add(label, 0, slotIndex);
-			slotIndex++;
-		}
-
-		ScrollPane scroller = new ScrollPane(calendar);
-
-		Scene scene = new Scene(scroller);
-		scene.getStylesheets().add(getClass().getResource("/calendar.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+                    calGrid.add(eventsToday, date.getDayOfWeek().getValue(), slotIndex);
+                    slotIndex++;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 	private static ResultSet getCalendarContent(LocalDate basis, LocalDate startOfWeek,
 												LocalDate endOfWeek, DatabaseHandler dbh) throws SQLException {
@@ -132,8 +88,48 @@ public class Calendar extends Application {
 		return prepStatement.executeQuery();
 	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+	private Button concertButton(VBox eventsToday, ResultSet rs) throws SQLException {
+        String concertText = rs.getString(4) + '\n' + rs.getString(5) + '\n' + rs.getString(6);
+        Button newBtn = new ConcertButton(concertText, rs.getInt(1));
 
+        //TODO modify states according to usecase
+        switch (rs.getString(6)) {
+            case "pending":
+                newBtn.getStyleClass().addAll("concert", "pending");
+                break;
+            case "accepted":
+                newBtn.getStyleClass().addAll("concert", "accepted");
+                break;
+            case "sent":
+                newBtn.getStyleClass().addAll("concert", "sent");
+                break;
+            case "declined":
+                newBtn.getStyleClass().addAll("concert", "declined");
+                break;
+        }
+        return newBtn;
+    }
+
+	private void setDateLabels() {
+        DateTimeFormatter dayFormatter = DateTimeFormatter.ofPattern("E\nMMM d");
+
+        for (LocalDate date = startOfWeek; !date.isAfter(endOfWeek); date = date.plusDays(1)) {
+            Label label = new Label(date.format(dayFormatter));
+            label.setPadding(new Insets(1));
+            label.setTextAlignment(TextAlignment.CENTER);
+            GridPane.setHalignment(label, HPos.CENTER);
+            calGrid.add(label, date.getDayOfWeek().getValue(), 0);
+        }
+    }
+
+    private void setStageLabels() {
+        int slotIndex = 1;
+        for (stages stage : stages.values()) {
+            Label label = new Label(stage.toString());
+            label.setPadding(new Insets(2));
+            GridPane.setHalignment(label, HPos.RIGHT);
+            calGrid.add(label, 0, slotIndex);
+            slotIndex++;
+        }
+    }
 }
---
+++ src/main/java/com/it1901/booking/JavaFX/Calendar/ConcertButton.java
@@ -0,0 +1,17 @@
+package com.it1901.booking.JavaFX.Calendar;
+
+import javafx.scene.control.Button;
+
+public class ConcertButton extends Button {
+
+    //need to store concertID in case user clicks on the button to see details
+    //about the concert
+    protected final Integer concertID;
+
+    public ConcertButton(String text, Integer concertID) {
+        super(text);
+        this.concertID = concertID;
+    }
+
+
+}
