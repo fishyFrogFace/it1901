7ddc95061ef036c2136e4930c75877af59bcb93d
Philip <philippu@stud.ntnu.no>
2016-10-04 17:28:04 +0200

    merge

--- src/main/java/com/it1901/booking/JavaFX/Controllers/ArtistViewController.java
+++ src/main/java/com/it1901/booking/JavaFX/Controllers/ArtistViewController.java
@@ -1,18 +1,14 @@
 package com.it1901.booking.JavaFX.Controllers;
 
-import com.it1901.booking.Application.DatabaseHandler;
 import com.it1901.booking.Application.SearchHandler;
 import com.it1901.booking.Application.TableViewMaker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class ArtistViewController extends Controller {
 
