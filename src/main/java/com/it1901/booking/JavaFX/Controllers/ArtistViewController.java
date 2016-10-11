7fe5ee9f8df9d70cf6995b7fa0b1c59b5713538a
Philip <philippu@stud.ntnu.no>
2016-10-11 13:00:25 +0200

    added searchbar to artistview

--- src/main/java/com/it1901/booking/JavaFX/Controllers/ArtistViewController.java
+++ src/main/java/com/it1901/booking/JavaFX/Controllers/ArtistViewController.java
@@ -3,8 +3,11 @@ package com.it1901.booking.JavaFX.Controllers;
 import com.it1901.booking.Application.SearchHandler;
 import com.it1901.booking.Application.TableViewMaker;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import org.controlsfx.control.textfield.TextFields;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -36,6 +39,12 @@ public class ArtistViewController extends Controller {
     @FXML
     private AnchorPane tableAnchor;
 
+    @FXML
+    private TextField searchArtistField;
+
+    @FXML
+    private Button searchArtistButton;
+
     public void displayArtist(String artist) {
 
         try {
@@ -52,9 +61,13 @@ public class ArtistViewController extends Controller {
                 System.out.println("artist not found");
             }
             ResultSet concertRS = SearchHandler.getPreviousConcerts(artist, app.getDatabaseHandler());
-            tableAnchor.getChildren().add(TableViewMaker.makeTable(concertRS, Arrays.asList("ID", "Duration", "Price", "Sold", "Stage")));
+            tableAnchor.getChildren().add(TableViewMaker.makeTable(concertRS, Arrays.asList("ID", "Duration", "Price", "Sold", "Stage, Stage name")));
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public void getAuto() throws SQLException {
+        // TextFields.bindAutoCompletion(searchArtistField, SearchHandler.getCollection("artist", "name", app.getDatabaseHandler()));
     }
 }
