02af2a5594efbead7a8c2d98071d9c3b1cdefc83
KimRobin <KimRobin@10.22.44.106>
2016-10-21 13:09:48 +0200

    test

--- src/main/java/com/it1901/booking/Application/ArtistInfo.java
+++ src/main/java/com/it1901/booking/Application/ArtistInfo.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.text.DecimalFormat;
 
 public class ArtistInfo {
-
+	//TEST
 	private DecimalFormat formatter = new DecimalFormat("#,###");
 
 	public String getArtistInfo(String artist, DatabaseHandler dbh) throws SQLException {
