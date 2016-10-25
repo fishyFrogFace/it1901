53a263a67b469303bf682b121cb8c52cb25e9a75
fishyFrogFace <camillahallo@mail.com>
2016-10-25 12:27:19 +0200

    Add insert to requirements

--- src/main/java/com/it1901/booking/JavaFX/Controllers/RequirementsController.java
+++ src/main/java/com/it1901/booking/JavaFX/Controllers/RequirementsController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class RequirementsController extends Controller {
@@ -23,6 +24,8 @@ public class RequirementsController extends Controller {
     @FXML
     private TextArea comments;
 
+    //TODO add messageText
+
     public void onLoad() {
         try {
             artists.getItems().setAll(Artist.getArtistNames(app.getDatabaseHandler()));
@@ -32,7 +35,17 @@ public class RequirementsController extends Controller {
     }
 
     public void storeRequirement(ActionEvent actionEvent) {
-        String query = "INSERT INTO requirements VALUES" +
-                "(DEFAULT, ?, ?, ?, ?)";
+        String query = "INSERT INTO requirement VALUES" +
+                "(DEFAULT, ?, ?, ?)";
+        try {
+            PreparedStatement prepStatement = app.getDatabaseHandler().prepareQuery(query);
+            prepStatement.setString(1, artists.getValue());
+            prepStatement.setString(2, description.getText());
+            prepStatement.setString(3, comments.getText());
+            prepStatement.executeUpdate();
+        } catch (SQLException e) {
+            //TODO catch all exceptions and do something with them
+            e.printStackTrace();
+        }
     }
 }
