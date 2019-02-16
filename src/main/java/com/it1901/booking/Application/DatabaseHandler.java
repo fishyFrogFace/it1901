588e6445af88af2884b9d7c3f9b76fbc71ae2987
Camilla Rygh <camillahallo@mail.com>
2019-02-16 19:20:19 +0100

    Remove redundant null check

--- src/main/java/com/it1901/booking/Application/DatabaseHandler.java
+++ src/main/java/com/it1901/booking/Application/DatabaseHandler.java
@@ -9,12 +9,6 @@ public class DatabaseHandler {
         public DatabaseHandler(String dbclass, String host, String user, String pass) throws SQLException, ClassNotFoundException {
             Class.forName(dbclass);
             this.con = DriverManager.getConnection(host, user, pass);
-            if (con != null) {
-                System.out.println("Connected to database.\n");
-            }
-            else {
-                System.out.println("Could not establish a connection to the database.\n");
-            }
         }
 
         public PreparedStatement prepareQuery(String query) throws SQLException {
